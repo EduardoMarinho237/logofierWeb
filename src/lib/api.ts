75f2c521692ca057c15bea2f43e61a53ab8318dd
Eduardo Marinho <eduardo.bisaweb@gmail.com>
2026-09-08 17:39:28 -0300

    fix vercel limit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 "use client";
 
-const API_URL = "";
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
 
 const TOKEN_KEY = "logofier_token";
 const REFRESH_TOKEN_KEY = "logofier_refresh_token";
